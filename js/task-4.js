84b7c5d9ea3745bf681142e54a7921c56c9a3e1e
Vita20 <vitahappy20@gmail.com>
2025-03-27 01:54:52 +0200

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+
+// const text = ["Hello", "world", "!"];
+
+// console.log(text.join(" ")); // "Hello world !" // об'єднує масив в строку, розділяючи пробілом
+// console.log(text.join("")); // "Helloworld!" // об'єднує масив в строку, без розділювача
+// console.log(text.join("-")); // "Hello-world-!"  // об'єднує масив в строку, розділяючи тире   
+// console.log(text.join("_")); // "Hello_world_!" // об'єднує масив в строку, розділяючи підкреслення
+// console.log(text.join()); // "Hello,world,!" // якщо не вказувати розділювач, то використовується кома 
+// console.log(text.join(",")); // "Hello,world,!" // об'єднує масив в строку, розділяючи комою
+
+
+
+// const text2 = 'Hello, world, !';
+// console.log(text2.split(" ")); // ['Hello,', 'world,', '!'] // розділює по пробілу
+// console.log(text2.split("")); // ['H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', ',', ' ', '!'] // розділює по символу
+// console.log(text2.split(", ")); // ['Hello', 'world', '!'] // розділює по комі
+// console.log(text2.split(","));  // ['Hello', ' world', ' !'] // розділює по комі
+// console.log(text2.split()); // ['Hello, world, !']  // якщо не вказувати розділювач, то повертає масив з одним елементом
+// console.log(text2.split("-")); // ['Hello, world, !'] // якщо розділювач не знайдений, то повертає масив з одним елементом

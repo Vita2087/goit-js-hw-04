b9542462519f460d94fd557e163d71350606bc39
Vita20 <vitahappy20@gmail.com>
2025-04-03 23:20:41 +0300

    done task3

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 // Задача 1. Пакування товарів
-// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, 
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює,
 // чи помістяться всі товари в контейнер при пакуванні.
 
 // Функція оголошує два параметри:
@@ -11,25 +11,12 @@
 // Тобто порахувати загальну кількість товарів в об'єкті products і повернути true,
 //  вона менше або дорівнює containerSize, і false, якщо ні.
 
-function isEnoughCapacity(products, containerSize) {
-  
-}
+function isEnoughCapacity(products, containerSize) {}
 
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-); // false
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -15,13 +15,20 @@
 // де <Username> — це ім'я профілю, а <amount> — кількість ігрових годин.
 
 const profile = {
-  username: 'Jacob',
+  username: "Jacob",
   playTime: 300,
-  changeUsername(newName) {},
-  updatePlayTime(hours) {},
-  getInfo() {},
+    changeUsername(newName) {
+        this.username = newName;
+  },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+  },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 
+// console.log(profile.changeUsername);
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
 profile.changeUsername('Marco');
